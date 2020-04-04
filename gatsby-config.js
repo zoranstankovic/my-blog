d3fd8df8441e67686bda0a986dbd7daca093f4e6
Zoran <zoranstankovic24@gmail.com>
2020-04-04 10:09:18 +0200

    Added posts
    Added posts and a post preview on home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,5 +11,22 @@ module.exports = {
     description:
       "A personal blog about programming, book reviews, and much more!",
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+        },
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: "posts",
+      },
+    },
+  ],
 }
